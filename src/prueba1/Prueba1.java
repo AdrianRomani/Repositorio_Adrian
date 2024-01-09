7ce6dc6da9151dd4b91fee47839283ab918857f5
roman <roman@HP>
2024-01-09 13:06:41 -0500

    first commit

--- src/prueba1/Prueba1.java
+++ src/prueba1/Prueba1.java
@@ -1,20 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package prueba1;
-
-/**
- *
- * @author roman
- */
+import java.util.Scanner;
 public class Prueba1 {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int a,b,area;
+        Scanner entrada= new Scanner (System.in);
+        System.out.println("Ingrese el largo y ancho del rectangulo");
+        a=entrada.nextInt();
+        b=entrada.nextInt();
+        
+        area=a*b;
+        
+        System.out.println("El area del rectangulo es "+area);
+
     }
     
 }
